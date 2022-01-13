ed1cbe6eff8f607215c4133e674f1d62c3e91f03
GironeA22 <gironea22@gmail.com>
2022-01-13 14:49:57 -0700

    Functioning create chatroom modal, need to add working room creation

---
+++ src/components/Customchats.js
@@ -0,0 +1,19 @@
+// import CreateChatroom from "./homepage/CreateChatroom";
+// import CustomchatModal from "./Customchat/CustomchatModal";
+// import { useState } from "react";
+
+// const Customchat = () => {
+// // const [modalShow, setModalShow] = useState(false);
+// // const handleShow = () => setModalShow(true);
+
+// // function handleSubmit(e) {
+// //     e.preventDefault()
+// //     handleShow();
+// // }
+
+//  <CreateChatroom onClick={}>
+//      <CustomchatModal show={modalShow} onHide={() => setModalShow(false)}  />
+//  </CreateChatroom>
+// }
+
+// export default Customchat;
--- src/components/homepage/CreateChatroom.jsx
+++ src/components/homepage/CreateChatroom.jsx
@@ -1,14 +1,25 @@
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
+import React, { useState } from 'react';
+import CustomchatModal from "../Customchat/CustomchatModal.js";
 
 const CreateChatroom = () => {
+    const [modalShow, setModalShow] = useState(false);
+    const handleShow = () => setModalShow(true);
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        handleShow();
+    }
+
     return (
         <>
             <Container fluid className="containers mb-5 pb-3">
                 <h1>Don't see a room that fits your groove?</h1>
-                <Button className="btn-secondary pt-3 px-5 mt-2">
+                <Button onClick={handleSubmit} modalShow={modalShow} setModalShow={setModalShow} className="btn-secondary pt-3 px-5 mt-2">
                     <h1>Create your own room</h1>
                 </Button>
+                <CustomchatModal show={modalShow} onHide={() => setModalShow(false)} />
             </Container>
         </>
     )
