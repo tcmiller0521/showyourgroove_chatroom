050c87607bb8eef18e2a40d233f454ab9a0a7a52
GironeA22 <gironea22@gmail.com>
2022-01-23 14:57:32 -0700

    Hit a brick wall with custom rooms

--- client/src/components/chatrooms/customchat/CustomchatModal.js
+++ client/src/components/chatrooms/customchat/CustomchatModal.js
@@ -1,6 +1,30 @@
 import { Modal, Button, Form } from 'react-bootstrap';
+import { addContent, selectContent } from '../../../state/contentSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import {useState} from 'react';
+
+function SubmitHandler() {
+    const dispatch = useDispatch();
+    const content = useSelector(selectContent);
+    const [response, setResponse] = useState({ title: "" })
+    const newRoom = [
+        ...content, response];
+
+    if (response.title) {
+        dispatch(addContent(newRoom))
+        setResponse({})
+    }
+
+    const updateField = (e) => {
+        setResponse({
+            ...response,
+            [e.target.title]: e.target.value
+        })
+    }
+}
 
 function CustomchatModal(props) {
+
     return (
         <Modal
             {...props}
@@ -14,11 +38,12 @@ function CustomchatModal(props) {
                 </Modal.Title>
             </Modal.Header>
             <Form className='bg-secondary'>
-                <Form.Control className="bg-light m-1 text-secondary" size="lg" type="text" placeholder="Room name" />
-                <Form.Control type='color' />
+                <Form.Control className="bg-light m-1 text-secondary" size="lg" type="text" placeholder="Room name" onChange={updateField} />
+                <Form.Control type='color' onChange={updateField} />
             </Form>
             <Modal.Footer>
                 <Button onClick={props.onHide}>Close</Button>
+                <Button onClick={SubmitHandler}>Submit</Button>
             </Modal.Footer>
         </Modal>
     );
